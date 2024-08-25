4e27b9ea7aa78827f43766830f7c2d4fb3e36179
sandersletvold <143513259+sandersletvold@users.noreply.github.com>
2024-08-25 20:23:45 +0200

    Fortsatt på oppgavene for mandag 26. aug.

--- 1.3OrdnedeTabeller/src/ordnede_tabeller/_1_3_2.java
+++ 1.3OrdnedeTabeller/src/ordnede_tabeller/_1_3_2.java
@@ -2,6 +2,13 @@ package ordnede_tabeller;
 
 public class _1_3_2 {
     // Oppgave 1
+    // [3,1], [3,2], [5,4], [5,1], [5,2], [4,1], [4,2], [7,6], [7,1], [7,2], [6,1], [6,2], [8,1], [8,2]
+    // Dette blir 14 inversjoner
 
     // Oppgave 2
+    // 1,3,2,10,9,8,7,6,5,4
+    // 1+6+5+4+3+2+1 blir tilsammen 22 inversjoner
+
+    // 10+9+1+8+2+3+4+5+6+7
+    // 9+8+6 blir tilsammen 23 inversjoner
 }
--- 1.3OrdnedeTabeller/src/ordnede_tabeller/_1_3_3.java
+++ 1.3OrdnedeTabeller/src/ordnede_tabeller/_1_3_3.java
@@ -1,7 +1,50 @@
 package ordnede_tabeller;
 
+import java.util.Arrays;
+import java.util.Random;
+
 public class _1_3_3 {
+    public static void main(String[] args) {
+        System.out.println(boble(randPerm(10)));
+    }
     // Oppgave 1
+    public static int boble(int[] a)      // legges i samleklassen Tabell
+    {
+        int antall = 0;                     // antall ombyttinger i tabellen
+        for (int i = 1; i < a.length; i++)  // starter med i = 1
+        {
+            if (a[i - 1] > a[i])              // sammenligner to naboverdier
+            {
+                System.out.println(a[i-1]+" byttet med "+a[i]);
+                bytt(a, i - 1, i);              // bytter om to naboverdier
+                antall++;                       // teller opp ombyttingene
+            }
+        }
+        return antall;                      // returnerer
+    }
+
+    public static void bytt(int[] a, int i, int j) {
+        int temp = a[i];
+        a[i] = a[j];
+        a[j] = temp;
+    }
+
+    public static int[] randPerm(int n)  // en effektiv versjon
+    {
+        Random r = new Random();         // en randomgenerator
+        int[] a = new int[n];            // en tabell med plass til n tall
+
+        Arrays.setAll(a, i -> i + 1);    // legger inn tallene 1, 2, . , n
+
+        for (int k = n - 1; k > 0; k--)  // løkke som går n - 1 ganger
+        {
+            int i = r.nextInt(k+1);        // en tilfeldig tall fra 0 til k
+            bytt(a,k,i);                   // bytter om
+        }
+
+        return a;                        // permutasjonen returneres
+    }
+
 
     // Oppgave 2
 
