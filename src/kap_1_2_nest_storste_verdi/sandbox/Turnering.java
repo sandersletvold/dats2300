af7f014926d46dd5dbbe3297d5125151d3080a57
sandersletvold <143513259+sandersletvold@users.noreply.github.com>
2024-08-29 20:30:17 +0200

    Fullført formateringsmetoder for turneringen.

--- src/kap_1_2_nest_storste_verdi/sandbox/Turnering.java
+++ src/kap_1_2_nest_storste_verdi/sandbox/Turnering.java
@@ -3,15 +3,12 @@ package kap_1_2_nest_storste_verdi.sandbox;
 import lib.Metoder;
 import lib.Sandbox;
 
-import java.util.Arrays;
-
 public class Turnering {
     public static void main(String[] args) {
-        int[] a = Metoder.randPerm(8);
+        int[] a = Metoder.randPerm(1024);
         int[] b = Sandbox.turnering(a);
-        // System.out.println(Arrays.toString(b));
+        Sandbox.turneringFormatering(b);
+        System.out.println();
         Sandbox.turneringVinner(b);
-        System.out.println("\n"+Arrays.toString(a));
-        // Sandbox.turneringFormatering(b);
     }
 }
